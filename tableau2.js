88660a86282a2f196ebb851e38e5081182992e2a
aaronwky <aaron.wong.2021@scis.smu.edu.sg>
2024-04-05 03:38:45 +0800

    dwadsa

--- tableau2.js
+++ tableau2.js
@@ -1,6 +1,6 @@
 function initViz() {
     var containerDiv = document.getElementById("tableauViz"),
-    url = "https://public.tableau.com/views/Trash_Dashboard/Dashboard4?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link";
+    url = "https://public.tableau.com/views/Trash_Dashboard/Dashboard12?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link";
 
     var viz = new tableau.Viz(containerDiv, url);
 }
